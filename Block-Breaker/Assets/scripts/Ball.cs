f0afefb200a5a8068bc1249ebcfb1ccf0e3ac852
Kevin McNeil <dogwoodrawks@gmail.com>
2021-02-11 14:03:09 -0800

    added visuals and sound

--- Block-Breaker/Assets/scripts/Ball.cs
+++ Block-Breaker/Assets/scripts/Ball.cs
@@ -8,15 +8,21 @@ public class Ball : MonoBehaviour{
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
     //state
     Vector2 paddleToBallVector;
     bool hasStarted = false;
+
+    //Cached component references
+
+    AudioSource myAudioSource;
     
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource =  GetComponent<AudioSource>();
         
     }
 
@@ -43,4 +49,15 @@ public class Ball : MonoBehaviour{
             GetComponent<Rigidbody2D>().velocity = new Vector2 (xPush, yPush);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+       
+    }
+
 }
